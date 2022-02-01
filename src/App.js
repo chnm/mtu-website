d6d29c43c6e3ec3c1cfaf1dc05eb447792ac9556
Jason Heppler <jason.heppler@gmail.com>
2022-02-01 13:32:55 -0600

    initial map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import ReactMapGl, { Marker } from 'react-map-gl'
+import Geocoder from 'react-map-gl-geocoder'
+import 'mapbox-gl/dist/mapbox-gl.css'
+import 'react-map-gl-geocoder/dist/mapbox-gl-geocoder.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const mapboxToken = 'pk.eyJ1IjoiaGVwcGxlcmoiLCJhIjoiMjNqTEVBNCJ9.pGqKqkUDlcFmKMPeoARwkg'
+
+class Map extends Component {
+  constructor() {
+    super()
+    this.state = {
+      viewport: {
+        width: '100vw',
+        height: '100vh',
+        latitude: 38.8404,
+        longitude: -77.3152,
+        zoom: 13
+    },
+    currMarker: null,
+    markers: []
+  }
+  this.handleViewportChange = this.handleViewportChange.bind(this)
+  this.handleGeocoderViewportChange = this.handleGeocoderViewportChange.bind(this)
+  this.handleResult = this.handleResult.bind(this);
+  this.addMarker = this.addMarker.bind(this);
+}
+mapRef = React.createRef()
+
+handleViewportChange(viewport) {
+  this.setState(prevState => ({
+    viewport: {...prevState.viewport, ...viewport}
+  }))
+}
+handleGeocoderViewportChange(viewport) {
+  const geocoderDefaultOverrides = { transitionDuration: 1000 }
+
+  return this.handleViewportChange({
+    ...viewport,
+    ...geocoderDefaultOverrides
+  })
+}
+handleResult (result) {
+  this.setState({
+    currMarker: {
+      name: result.result.place_name,
+      latitude: result.result.center[1],
+      longitude: result.result.center[0]
+    }
+  })
+}
+addMarker() {
+  const {currMarker} = this.state
+  this.setState(prevState => ({
+    markers: [...prevState.markers, currMarker],
+    currMarker: null
+  }))
 }
 
-export default App;
+render() {
+  const {viewport, markers} = this.state
+
+  return (
+    <ReactMapGl
+    ref={this.mapRef}
+    {...viewport}
+    onViewportChange={viewport => this.setState({viewport})}
+    mapboxApiAccessToken={mapboxToken}
+    mapStyle="mapbox://styles/mapbox/streets-v9"
+    >
+
+      <button className="add-btn" onClick={this.addMarker}>Add Marker</button>
+      {markers.map((marker, idx) => {
+        return (
+          <Marker
+            key={idx}
+            latitude={marker.latitude}
+            longitude={marker.longitude}
+          >
+            <img src="pin.png" alt="pin" />
+            </Marker>
+        )
+      })
+      }
+
+      <Geocoder
+      onSelected={this.handleResult}
+      mapRef = {this.mapRef}
+      mapboxApiAccessToken={mapboxToken}
+      onResult={this.handleResult}
+      onViewportChange={this.handleGeocoderViewportChange}
+      countries="us"
+      position="top-right"
+      placeholder="Search for a location"
+      />
+    </ReactMapGl>
+  )
+}
+}
+
+export default Map;
