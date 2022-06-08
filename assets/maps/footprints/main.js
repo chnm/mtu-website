2683ce7fb50611cc1ccc69511a1527bebdd6b751
Jason Heppler <jason.heppler@gmail.com>
2022-06-08 14:27:07 -0500

    stand-alone building footprints page

---
+++ assets/maps/footprints/main.js
@@ -0,0 +1,84 @@
+mapboxgl.accessToken = "pk.eyJ1IjoiaGVwcGxlcmoiLCJhIjoiMjNqTEVBNCJ9.pGqKqkUDlcFmKMPeoARwkg";
+
+const map = new mapboxgl.Map({
+  container: "visualization",
+  style: "mapbox://styles/mapbox/streets-v11", // style URL
+  center: [-77.30871, 38.830272],
+  zoom: 15,
+});
+
+// disable map zoom when using scroll
+map.scrollZoom.disable();
+
+// Add zoom and rotation controls to the map.
+map.addControl(new mapboxgl.NavigationControl());
+
+// build a year array between 1964 and 2007
+const years = [];
+for (let i = 1964; i <= 2021; i++) {
+  years.push(i);
+}
+
+map.on("style.load", () => {
+  // load geojson data
+  map.addSource("gmu", {
+    type: "geojson",
+    data: "gmufootprints.geojson",
+  });
+
+  map.addLayer({
+    id: "footprints",
+    type: "fill",
+    source: "gmu",
+    layout: {},
+    paint: {
+      "fill-color": "brown",
+      "fill-opacity": 0.8,
+    },
+  });
+
+  // Show the name of the polygon on click
+  map.on('click', 'footprints', (e) => {
+    new mapboxgl.Popup()
+    .setLngLat(e.lngLat)
+    .setHTML(e.features[0].properties.NAME)
+    .addTo(map);
+    });
+
+  // Change the cursor to a pointer when
+  // the mouse is over the states layer.
+  map.on('mouseenter', 'footprints', () => {
+    map.getCanvas().style.cursor = 'crosshair';
+  });
+
+  // Change the cursor back to a pointer
+  // when it leaves the states layer.
+  map.on('mouseleave', 'footprints', () => {
+    map.getCanvas().style.cursor = '';
+    });
+
+  // When the year value is changed, update the map with the building footprints
+  // for the selected year and all previous years. In the geojson data, the year 
+  // is stored in the START_DATE and END_DATE property.
+  const yearSelect = document.getElementById("year-slider");
+  yearSelect.addEventListener("change", () => {
+    const year = yearSelect.valueAsNumber;
+    // append -01-01 to the year to get the start date
+    const yearStart = `${year}-01-01`;
+    // append -12-31 to the year to get the end date
+    const yearEnd = `${year}-12-31`;
+
+    // filter the geojson data to only include the buildings for the selected year and all previous years
+    const queryFilter = [
+      "all",
+      ["<=", ["get", "START_DATE"], yearStart],
+    ];
+    map.setFilter("footprints", queryFilter);
+  });
+});
+
+// Watch for the year-slider to change and update the 
+// year-range label with the value. 
+document.getElementById('year-slider').addEventListener('change', function(e) {
+  document.getElementById('year-range').innerHTML = e.target.value;
+});
---
+++ content/maps/footprints/index.md
@@ -0,0 +1,37 @@
+---
+title: "Building Footprints of George Mason University"
+date: 2022-05-31
+updated: 2022-05-31
+author: "Jason Heppler"
+abstract: "A piece of abstract text..."
+script: maps/footprints/main.js
+styles: maps/footprints/style.css
+layout: maps
+thumbnail: gmu_footprints.png
+thumbdesc: "A screenshot showing..."
+---
+
+Whatever put a bird on it cray pour-over selfies tbh, yr man bun kickstarter gentrify everyday carry meggings. Bitters poke tote bag tumeric 3 wolf moon twee. Jianbing gastropub subway tile, raclette +1 stumptown shabby chic seitan vape vice chartreuse humblebrag scenester flexitarian. Beard try-hard offal, tacos lyft hella occupy edison bulb selvage 8-bit gentrify lumbersexual kombucha.
+
+Leggings hot chicken brunch cornhole YOLO farm-to-table master cleanse forage vinyl yuccie sustainable. Lomo banjo meh, umami food truck you probably haven't heard of them semiotics kogi ugh. Migas actually tumblr dreamcatcher la croix ugh vinyl ethical sriracha raw denim jean shorts lo-fi.
+
+<label for="year-slider" id="year-range">1964</label><br/>
+<input id="year-slider"
+    type="range" 
+    min="1964" 
+    max="2021" 
+    step="1" 
+    value="0"
+    oninput="document.getElementById('year-range').innerHTML = this.value;"
+    class="w-3/4 h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700">
+<div id="visualization" style="height:500px; width:100%"></div>
+
+Cronut ennui tote bag, before they sold out kitsch vinyl hashtag swag ugh poutine vice activated charcoal. Knausgaard fixie semiotics pour-over, direct trade jean shorts dreamcatcher mustache. Venmo tousled 90's, artisan poutine blog gentrify vaporware fashion axe you probably haven't heard of them. Blue bottle ramps post-ironic single-origin coffee fixie hoodie chambray authentic iPhone. Sustainable literally pabst af. Asymmetrical umami deep v selfies enamel pin.
+
+Occupy tumeric hashtag, next level health goth PBR&B meditation. Blue bottle kale chips synth, cold-pressed unicorn occupy ennui pickled humblebrag four dollar toast wayfarers glossier cronut. Banjo subway tile 8-bit fam. Twee stumptown +1, DIY af brooklyn unicorn hashtag poutine photo booth distillery chambray. Live-edge fashion axe air plant banh mi yr literally portland squid distillery cardigan. Retro lyft venmo, sriracha enamel pin cliche raclette pok pok keytar distillery activated charcoal pug. 90's man braid vape farm-to-table.
+
+## Suggested citation
+
+Please use the following as a suggested citation:
+
+{{< citation >}}
