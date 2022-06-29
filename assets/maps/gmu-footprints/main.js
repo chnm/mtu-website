7f6a87e12f08355a547c2417d09a03a924a7c08a
Jason Heppler <jason.heppler@gmail.com>
2022-06-29 15:03:58 -0500

    access token fix

--- assets/maps/gmu-footprints/main.js
+++ assets/maps/gmu-footprints/main.js
@@ -1,4 +1,4 @@
-mapboxgl.accessToken = "pk.eyJ1IjoiaGVwcGxlcmoiLCJhIjoiY2w0enp6dDljM2RmcjNicWM3cmw3ajMwNiJ9.Pchp6cBZnn8qHIyKx80MTA";
+mapboxgl.accessToken = "pk.eyJ1IjoiaGVwcGxlcmoiLCJhIjoiMjNqTEVBNCJ9.pGqKqkUDlcFmKMPeoARwkg";
 
 const map = new mapboxgl.Map({
   container: "visualization",
